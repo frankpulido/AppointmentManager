bafb2a4a1e7b2b7cd42a97386a0b1a2508f9e1bd
frankpulido <frankpulido@me.com>
2025-08-25 12:14:06 +0200

    improved get methods for frontend practitioner filtering

--- app/Http/Controllers/AvailableSlotsController.php
+++ app/Http/Controllers/AvailableSlotsController.php
@@ -5,20 +5,49 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\AvailableTimeSlotDiagnosis;
 use App\Models\AvailableTimeSlot;
+use App\Models\Practitioner;
 
 class AvailableSlotsController extends Controller
 {
     public function index90()
     {
         // Logic to display available slots for 90-minute appointments
+        $practitioners = Practitioner::get()->mapWithKeys(function($p) {
+            return [$p->id => $p->first_name . ' ' . $p->last_name];
+        })->toArray();
+
         $availableSlots90 = AvailableTimeSlotDiagnosis::all();
-        return response()->json($availableSlots90, 200);
+        $availableSlots90Filtered = [];
+
+        foreach($availableSlots90 as $slot) {
+            $availableSlots90Filtered[$slot->practitioner_id][] = $slot;
+        }
+
+        return response()->json([
+            'practitioners' => $practitioners,
+            'availableSlots90Filtered' =>  $availableSlots90Filtered],
+            200
+        );
     }
 
     public function index60()
     {
         // Logic to display available slots for 60-minute appointments
+        $practitioners = Practitioner::get()->mapWithKeys(function($p) {
+            return [$p->id => $p->first_name . ' ' . $p->last_name];
+        })->toArray();
+
         $availableSlots60 = AvailableTimeSlot::all();
-        return response()->json($availableSlots60, 200);
+        $availableSlots60Filtered = [];
+
+        foreach($availableSlots60 as $slot) {
+            $availableSlots60Filtered[$slot->practitioner_id][] = $slot;
+        }
+
+        return response()->json([
+            'practitioners' => $practitioners,
+            'availableSlots90Filtered' =>  $availableSlots60Filtered],
+            200
+        );
     }
 }
