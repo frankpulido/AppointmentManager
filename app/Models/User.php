37046754562d126a3084a24ac410aaf2ab1e0e91
frankpulido <frankpulido@me.com>
2025-09-04 16:00:51 +0200

    modified user model-migration for roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    public const VALID_ROLES = ['admin', 'practitioner'];
     /**
      * The attributes that are mass assignable.
      *
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -17,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('practitioner');
+            $table->enum('role', User::VALID_ROLES)->default('practitioner');
             $table->foreignId('practitioner_id')
                 ->nullable()
                 ->constrained('practitioners')
