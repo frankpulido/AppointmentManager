4cf4d4a8dd2e36704694db340d97a191f1cf1386
frankpulido <frankpulido@me.com>
2025-09-24 11:07:27 +0200

    modified slot controller for customized max_days_ahead purging practitioner from response

--- app/Http/Controllers/AvailableSlotsController.php
+++ app/Http/Controllers/AvailableSlotsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AvailableTimeSlotDiagnosis;
 use App\Models\AvailableTimeSlot;
 use App\Models\Practitioner;
-use App\Models\Appointment;
+//use App\Models\Appointment;
 
 class AvailableSlotsController extends Controller
 {
@@ -15,23 +15,45 @@ class AvailableSlotsController extends Controller
         $practitioners = Practitioner::get()->mapWithKeys(function($p) {
             return [$p->id => $p->first_name . ' ' . $p->last_name];
         })->toArray();
+        
+        $all_diagnosis_slots = AvailableTimeSlotDiagnosis::with('practitioner')->get();
 
+        $available_diagnosis_slots_by_practitioner = $all_diagnosis_slots->filter(function ($slot) {
+            $max_days_ahead = $slot->practitioner->getPractitionerSetting('max_days_ahead');
+            return $slot->slot_date <= now()->addDays($max_days_ahead);
+        })
+        ->each(function ($slot) {
+            $slot->makeHidden('practitioner');
+        })
+        ->groupBy('practitioner_id');
+
+        /*
         $practitioners_availability = Practitioner::all();
-        $availableSlots90 = collect();
+        $available_diagnosis_slots_by_practitioner = collect();
         foreach($practitioners_availability as $practitioner) {
             $max_days_ahead = $practitioner->getPractitionerSetting('max_days_ahead');
             $slots = AvailableTimeSlotDiagnosis::query()
                 ->where('practitioner_id', $practitioner->id)
                 ->where('slot_date', '<=', now()->addDays($max_days_ahead)->toDateString())
                 ->get();
-            $availableSlots90->put($practitioner->id, $slots);
+            $available_diagnosis_slots_by_practitioner->put($practitioner->id, $slots);
         }
 
-        $availableSlots90Filtered = $availableSlots90->toArray();
+        $available_diagnosis_slots_by_practitioner_array = $available_diagnosis_slots_by_practitioner->toArray();
+        */
 
         return response()->json([
             'practitioners' => $practitioners,
-            'available_slots_diagnose' =>  $availableSlots90Filtered],
+            /*
+            'available_slots_diagnose' =>  $available_diagnosis_slots_by_practitioner->only([
+                'id',
+                'practitioner_id',
+                'slot_date',
+                'slot_start_time',
+                'slot_end_time',
+            ])],
+            */
+            'available_slots_diagnose' =>  $available_diagnosis_slots_by_practitioner->toArray()],
             200
         );
     }
@@ -43,6 +65,18 @@ class AvailableSlotsController extends Controller
             return [$p->id => $p->first_name . ' ' . $p->last_name];
         })->toArray();
 
+        $all_treatment_slots = AvailableTimeSlot::with('practitioner')->get();
+ 
+        $available_treatment_slots_by_practitioner = $all_treatment_slots->filter(function ($slot) {
+            $max_days_ahead = $slot->practitioner->getPractitionerSetting('max_days_ahead');
+            return $slot->slot_date <= now()->addDays($max_days_ahead);
+        })
+        ->each(function ($slot) {
+            $slot->makeHidden('practitioner');
+        })
+        ->groupBy('practitioner_id');
+
+        /*
         $practitioners_availability = Practitioner::all();
         $availableSlotsTreatment = collect();
         foreach($practitioners_availability as $practitioner) {
@@ -55,10 +89,11 @@ class AvailableSlotsController extends Controller
         }
 
         $availableSlotsTreatmentFiltered = $availableSlotsTreatment->toArray();
+        */
 
         return response()->json([
             'practitioners' => $practitioners,
-            'available_slots_treatment' =>  $availableSlotsTreatmentFiltered],
+            'available_slots_treatment' =>  $available_treatment_slots_by_practitioner->toArray()],
             200
         );
     }
